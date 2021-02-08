21766cb4d4edb99a3f741b0eb7b9797229607900
om-mapari <76249235+om-mapari@users.noreply.github.com>
2021-02-08 06:43:13 +0530

    Update 2.e)Integer_break.cpp

--- Dynamic Programming/2.e)Integer_break.cpp
+++ Dynamic Programming/2.e)Integer_break.cpp
@@ -6,20 +6,17 @@ https://leetcode.com/problems/integer-break/
 class Solution {
 public:
     int integerBreak(int n) {
-        int dp[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        
-        for(int i=2;i<=n;i++){
-            dp[i] = INT_MIN;
-        }
-        
-        for(int i=2;i<=n;i++){
-            for(int j=1;j<i;j++){
-                dp[i] = max(dp[i], max(dp[i-j], i-j)*j);
-            }
-        }
-        
-        return dp[n];
+    if (n == 2)
+        return 1;
+    if (n == 3)
+        return 2;
+    int prod = 1;
+    while (n > 4)
+    {
+        prod *= 3;
+        n = n - 3;
+    }
+    prod *= n;
+    return prod;
     }
 };
